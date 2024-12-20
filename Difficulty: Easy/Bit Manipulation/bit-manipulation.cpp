61ad673070222b4f6fde08dc263400f2ab2fc2a6
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2024-12-20 15:20:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bit Manipulation/bit-manipulation.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void bitManipulation(int num, int i) {
+        // your code here
+        i--;
+        int ith=(num>>i)&1;
+        int setith=(num | (1<<i));
+        int clearit=(num & ~(1<<i));
+        
+        cout<<ith<<" "<<setith<<" "<<clearit;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n >> i;
+        Solution ob;
+        ob.bitManipulation(n, i);
+        cout << "\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
