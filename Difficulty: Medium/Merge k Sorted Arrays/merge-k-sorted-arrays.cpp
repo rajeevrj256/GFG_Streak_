a7ccacc456cfbdf7d76f1ee5d60d0f85b3d2cba1
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2024-12-29 23:21:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+#define N 105
+using namespace std;
+void printArray(vector<int> arr, int size)
+{
+for (int i=0; i < size; i++)
+	cout << arr[i] << " ";
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+    public:
+    //Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
+    {
+        //code here
+        vector<int>merge_sorted_array;
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr[0].size();j++){
+                merge_sorted_array.push_back(arr[i][j]);
+            }
+        }
+        
+        sort(merge_sorted_array.begin(),merge_sorted_array.end());
+        return merge_sorted_array;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+	    int k;
+	    cin>>k;
+	    vector<vector<int>> arr(k, vector<int> (k, 0));
+	    for(int i=0; i<k; i++){
+	        for(int j=0; j<k; j++)
+	        {
+	            cin>>arr[i][j];
+	        }
+	    }
+	    Solution obj;
+    	vector<int> output = obj.mergeKArrays(arr, k);
+    	printArray(output, k*k);
+    	cout<<endl;
+    
+cout << "~" << "\n";
+}
+	return 0;
+}
+
+
+
+
+
+
+// } Driver Code Ends
