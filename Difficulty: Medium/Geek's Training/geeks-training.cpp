7125fbfdeddcebb3323a45c6a393efd574b36dcd
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-04-07 10:54:31 +0200

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -6,36 +6,23 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int t[100001][3];
-    int help(vector<vector<int>>& arr,int activity,int day){
-        if(day==arr.size())return 0;
-        
-        if(t[day][activity]!=-1)return t[day][activity];
-        
-        int sport1=0;
-        int sport2=0;
-        int sport3=0;
-        
-        if(activity==0){
-            sport1=arr[day][0]+max(help(arr,1,day+1),help(arr,2,day+1));
-            
-        }
-        
-        if(activity==1){
-            sport2=arr[day][1]+max(help(arr,0,day+1),help(arr,2,day+1));
-        }
-        
-        if(activity==2){
-            sport2=arr[day][2]+max(help(arr,0,day+1),help(arr,1,day+1));
-        }
-        
-        return t[day][activity]=max({sport1,sport2,sport3});
-    }
+   
     int maximumPoints(vector<vector<int>>& arr) {
         // Code here
-        memset(t,-1,sizeof(t));
+        //memset(t,-1,sizeof(t));
+        int n=arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(3,0));
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
         
-        return max({help(arr,0,0),help(arr,1,0),help(arr,2,0)});
+        for(int i=1;i<n;i++){
+            dp[i][0]=arr[i][0]+max(dp[i-1][1],dp[i-1][2]);
+            dp[i][1]=arr[i][1]+max(dp[i-1][0],dp[i-1][2]);
+            dp[i][2]=arr[i][2]+max(dp[i-1][0],dp[i-1][1]);
+            
+        }
+        return max({dp[n-1][0],dp[n-1][1],dp[n-1][2]});
         
     }
 };
