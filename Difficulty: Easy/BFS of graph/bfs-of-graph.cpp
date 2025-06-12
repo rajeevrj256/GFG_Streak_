43042ee522806ff37c1f14ec84ed5a780a678f3e
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-06-12 14:36:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,64 +1,25 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(vector<vector<int>> &adj) {
+    vector<int> bfs(vector<vector<int>> &adj) {
         // Code here
-        vector<int>ans;
-        unordered_map<int,bool>vis(false);
         queue<int>q;
         q.push(0);
-        ans.push_back(0);
-        vis[0]=true;
+        vector<int>ans;
+        int n=adj.size();
+        vector<int>vis(n,0);
+        vis[0]=1;
         while(!q.empty()){
-            int node=q.front();
+            int top=q.front();
             q.pop();
-            
-            for(auto neighbor:adj[node]){
-                if(!vis[neighbor]){
-                    vis[neighbor]=true;
-                    ans.push_back(neighbor);
-                    q.push(neighbor);
+            ans.push_back(top);
+            for(auto i:adj[top]){
+                if(!vis[i]){
+                    vis[i]=1;
+                q.push(i);
                 }
             }
         }
-        
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
-        // Now using vector of vectors instead of array of vectors
-        vector<vector<int>> adj(V);
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u); // For undirected graph, add both u->v and v->u
-        }
-
-        Solution obj;
-        vector<int> ans = obj.bfsOfGraph(adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
